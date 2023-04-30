7a7c9cbca78e85df8d693f14e999da9a62bc0933
AMIT KUMAR <65363024+Kumarsonu143668@users.noreply.github.com>
2023-04-30 21:51:00 +0530

    Add files via upload

---
+++ src/test/java/com/supremeshop/platform/SupremeshopConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supremeshop.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupremeshopConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
